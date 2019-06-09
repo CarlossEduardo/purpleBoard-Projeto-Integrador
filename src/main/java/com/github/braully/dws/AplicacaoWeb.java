724117390cc87257e4afc78dac6aab894b6f4dc6
Carlos Eduardo <Carlos Eduardo@DESKTOP-5H846O8>
2019-06-09 17:32:20 -0300

    comitandooo

--- src/main/java/com/github/braully/dws/AplicacaoWeb.java
+++ src/main/java/com/github/braully/dws/AplicacaoWeb.java
@@ -23,27 +23,94 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.context.ServletContextAware;
+
+
+
+import com.sun.faces.config.ConfigureListener;
+import static controlizinho.ConsultasDao.GerarTabela2;
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import modelinho.TabelaConsulta2;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.context.ServletContextAware;
 
 /**
  *
  * @author braully
  */
+@EnableWebSecurity
 @SpringBootApplication
-public class AplicacaoWeb implements ServletContextAware {
+public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements ServletContextAware {
 
     public static void main(String... args) throws Exception  {
         SpringApplication.run(AplicacaoWeb.class, args);
-        ArrayList<Cliente1> listona = listarCliente();
-        System.out.println(listona.get(0).getNomeCliente());
-        ArrayList<TabelaVendaLojaSemestre> tt = ValorVendasLojaSemestre();
-        System.out.println(tt.get(0).getNomeLoja());
-        Object[] valorQuantidade = QtdeVendasSemestre();
-        System.out.println(valorQuantidade[0] + "  "+ valorQuantidade[1]);
-        int quantidadeLoja = QuantidadeLoja();
-        System.out.println("essa é qtd loja "+ quantidadeLoja);
+        
+        ArrayList<TabelaConsulta2> listona = GerarTabela2("2019", "1");
+        System.out.println(listona.get(0).getNome());
+       // System.out.println(listona.get(0).getNomeCliente());
+       // ArrayList<TabelaVendaLojaSemestre> tt = ValorVendasLojaSemestre();
+       // System.out.println(tt.get(0).getNomeLoja());
+       // Object[] valorQuantidade = QtdeVendasSemestre();
+       // System.out.println(valorQuantidade[0] + "  "+ valorQuantidade[1]);
+       // int quantidadeLoja = QuantidadeLoja();
+       // System.out.println("essa é qtd loja "+ quantidadeLoja);
   
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+       // UserDetails user = User.withUsername("usuario")
+       //         .password(passwordEncoder().encode("usuario"))
+       //         .roles("USER").build();
+
+        UserDetails admin = User.withUsername("gerente")
+                .password(passwordEncoder().encode("gerente"))
+                .roles("gerente").build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception  {
+         http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/cliente.xhtml").hasRole("gerente")
+                .anyRequest().permitAll().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+        http.csrf().disable();
+    }
+    
+    
+    
 
     @Bean
     public static CustomScopeConfigurer viewScope() {
@@ -72,6 +139,7 @@ public class AplicacaoWeb implements ServletContextAware {
                 new ConfigureListener());
     }
 
+   
     @Override
     public void setServletContext(ServletContext servletContext) {
         servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -251,7 +251,7 @@
     // Recent Report
     const brandProduct = 'rgba(0,181,233,0.8)'
     const brandService = 'rgba(0,173,95,0.8)'
-
+    
     var elements = 10
     var data1 = [52, 60, 55, 50, 65, 80, 57, 70, 105, 115]
     var data2 = [102, 70, 80, 100, 56, 53, 80, 75, 65, 90]
